b3932f6e3519ec475bdf0184f4969af8c7b01fdc
sergey37192 <31955756+sergey37192@users.noreply.github.com>
2018-03-28 05:31:47 +0300

    Небольшие правки

--- app/src/main/java/com/example/serge/test1/Settings.java
+++ app/src/main/java/com/example/serge/test1/Settings.java
@@ -13,8 +13,7 @@ import android.widget.LinearLayout;
 
 public class Settings {
     public static LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-    public static LinearLayout.LayoutParams textMessageViewParams = new LinearLayout.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-    public static LinearLayout.LayoutParams textAnwserViewParams = new LinearLayout.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
     public static LinearLayout.LayoutParams questionViewParams = new LinearLayout.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     public static LinearLayout.LayoutParams WaitingViewParams = new LinearLayout.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     public static LinearLayout.LayoutParams item = new LinearLayout.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT  );
@@ -22,9 +21,6 @@ public class Settings {
 
 
     static{
-        textMessageViewParams.setMargins(15,50,15,50);
-        textAnwserViewParams.setMargins(15,50,15,50);
-        textAnwserViewParams.gravity = Gravity.RIGHT;
         questionViewParams.setMargins( 25, 25, 25, 0 );
         item.setMargins( 50,0,0,0 );
     }
