469a9807bec568864bf2d1ae614557145209eeb9
sergey37192 <31955756+sergey37192@users.noreply.github.com>
2018-05-05 19:35:26 +0300

    Удалил ненужный метод

--- app/src/main/java/com/example/serge/test1/Scenario.java
+++ app/src/main/java/com/example/serge/test1/Scenario.java
@@ -33,69 +33,7 @@ public class Scenario {
     public static TreeMap<String, ArrayList<CustomEvents>> scenario = new TreeMap<>();
     public static TreeMap<String, Stage> scenarioList = new TreeMap<>();
     static String TAG = "MyLogScenario";
-/*
-    public static void loadSceanrio(Context context) throws XmlPullParserException, IOException{
-        Resources res = context.getResources();
-        XmlResourceParser parser = res.getXml(R.xml.scenario);
-        ArrayList<CustomEvents> events = null;
-        String tagName = null, stageName = null;
-        Stage stage = null;
-        Questions questions = null;
-        parser.next();
-        int eventType = parser.getEventType();
-        while(eventType != XmlPullParser.END_DOCUMENT){
 
-            if(eventType == XmlPullParser.START_TAG){
-                if((tagName = parser.getName()).equals("stage")){
-                    events = new ArrayList<>();
-                    stageName = parser.getAttributeValue( null, "id");
-                }
-                else{
-                    if(tagName.equals("message")){
-                        TextMessage mes = new TextMessage();
-                        mes.setText(parser.getAttributeValue( null, "text" ));
-                        events.add(mes);
-                    }else if(tagName.equals("important_message")){
-                        ImportantMessage importantMessage = new ImportantMessage();
-                        importantMessage.setText( parser.getAttributeValue( null, "text" ));
-                        events.add(importantMessage);
-                    }else if(tagName.equals( "questions" )){
-                        questions = new Questions();
-                    }else if(tagName.equals("case")){
-                        if(questions!=null){
-                            Question question = new Question();
-                            question.setText(parser.getAttributeValue( null, "text" ));
-                            question.setGoTo(parser.getAttributeValue( null, "target" ));
-                            questions.put(question);
-                        }
-                    }else if(tagName.equals("waiting")){
-                        Waiting waiting = new Waiting();
-                        waiting.setValue(  parser.getAttributeIntValue(null, "value", 2500) );
-                        events.add(waiting);
-                    }else if(tagName.equals("random_event")){
-                        RandomEvent randomEvent = new RandomEvent();
-                        randomEvent.setStage(parser.getAttributeValue(null, "stage"));
-                        events.add(randomEvent);
-                    }
-
-                }
-                Log.i(TAG, parser.getName());
-            }
-            if(eventType == XmlPullParser.END_TAG){
-                if(parser.getName().equals("stage")){
-                    if(events!=null&&events.size()!=0&&stageName!=null){
-                        scenario.put(stageName, events);
-                        events = null;
-                        stageName = null;
-                    }
-                }else if(parser.getName().equals( "questions" )){
-                    if(questions!=null&&questions.getList().size()!=0)
-                        events.add(questions);
-                }
-            }
-            eventType = parser.next();
-        }
-    }*/
 
     public static void loadSceanrio(Context context) throws XmlPullParserException, IOException{
         Resources res = context.getResources();
