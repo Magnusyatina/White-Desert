1fbcf3f5ccf17751426462552bd0d60a672ccd8c
sergey37192 <31955756+sergey37192@users.noreply.github.com>
2018-06-05 01:06:55 +0300

    Шторка

--- app/src/main/java/com/example/serge/test1/CustomView/CustomButtonPlayerAnswer.java
+++ app/src/main/java/com/example/serge/test1/CustomView/CustomButtonPlayerAnswer.java
@@ -2,15 +2,23 @@ package com.example.serge.test1.CustomView;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+
+import com.example.serge.test1.R;
+import com.example.serge.test1.Shared;
 
 /**
  * Created by sergey37192 on 08.04.2018.
  */
 
 public class CustomButtonPlayerAnswer extends android.support.v7.widget.AppCompatTextView{
+
     public CustomButtonPlayerAnswer(Context context) {
         super( context );
+
     }
 
     public CustomButtonPlayerAnswer(Context context, AttributeSet attrs) {
@@ -19,5 +27,11 @@ public class CustomButtonPlayerAnswer extends android.support.v7.widget.AppCompa
 
     public CustomButtonPlayerAnswer(Context context, AttributeSet attrs, int defStyleAttr) {
         super( context, attrs, defStyleAttr );
+    }
+
+    @Override
+    protected void onAnimationStart() {
+        super.onAnimationStart();
+        Log.d("MY", "animation start");
     }
 }
--- app/src/main/java/com/example/serge/test1/Engine.java
+++ app/src/main/java/com/example/serge/test1/Engine.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AlertDialog;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
