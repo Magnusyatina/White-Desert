fd6dce24b6b4094428cdd2b8f6bff7ad46eac9bf
sergey37192 <31955756+sergey37192@users.noreply.github.com>
2018-12-24 02:58:40 +0300

    Правки
    Небольшая работа с Vector. Внесены правки в сюжет

--- app/src/main/java/com/example/serge/test1/SplashActivity.java
+++ app/src/main/java/com/example/serge/test1/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.example.serge.test1;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by sergey37192 on 28.02.2018.
@@ -11,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate( savedInstanceState );
+        setContentView(R.layout.splash_activity);
+        ((Animatable)((ImageView)findViewById(R.id.splash_image)).getDrawable()).start();
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
